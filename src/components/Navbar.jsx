8ab3ec98787156985fcc364315143d21adc358bd
hari-0909 <146479573+hari-0909@users.noreply.github.com>
2024-11-08 01:02:37 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,25 @@
 import logo from "../assets/harilogo.jpg";
-import { FaLinkedin } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
+import { FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa";
 
 const Navbar = () => {
-  return <nav className="mb-20 flex items-center justify-between py-6">
-    <div className="flex flex-shrink-0 items-center">
-      <img className="mx-4 w-10" src={logo} alt="logo" />
-    </div>
-    <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-      <FaLinkedin />
-      <FaGithub/>
-      <FaInstagram/>
-    </div>
-  </nav>
+  return (
+    <nav className="mb-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <img className="mx-4 w-10" src={logo} alt="logo" />
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <a href="https://linkedin.com/in/hari-babuji-pasalapudi-47997b257/" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin />
+        </a>
+        <a href="https://github.com/hari-0909" target="_blank" rel="noopener noreferrer">
+          <FaGithub />
+        </a>
+        <a href="https://instagram.com/_hari._4" target="_blank" rel="noopener noreferrer">
+          <FaInstagram />
+        </a>
+      </div>
+    </nav>
+  );
 };
+
 export default Navbar;
